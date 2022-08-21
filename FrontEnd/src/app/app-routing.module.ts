1797147c2161856b1f1f8d22a8e576266e413f10
paulsalary <sueldopablod@gmail.com>
2022-08-21 20:17:41 -0300

    editar educacion agregado

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -10,6 +12,8 @@ const routes: Routes = [
   {path:'login',component : LoginComponent},
   {path:'nuevaexp', component: NewExperienciaComponent},
   {path:'editexp/:id', component: EditExperienciaComponent},
+  {path:'nuevaedu', component: NeweducacionComponent},
+  {path:'editedu/:id', component: EditeducacionComponent}
 ];
 
 @NgModule({
